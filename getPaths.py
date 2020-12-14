3cc4b97726cdb5b192cccbcdfeb1b6b964c73351
m4ll0k (mallok) <m4ll0k@protonmail.com>
2020-12-14 09:06:31 +0100

    Add files via upload

---
+++ getPaths.py
@@ -0,0 +1,15 @@
+import sys,html
+from urllib import parse
+from html import unescape
+
+allLinks = []
+
+def main(url):
+	url = url.replace(':80','')
+	parts = parse.urlparse(unescape(parse.unquote(url)))
+	final_url = parts.path + ('?' + parts.query if parts.query != '' else '') + ('#' + parts.fragment if parts.fragment != '' else '')
+	print(final_url)
+
+for i in sys.stdin.readlines():
+	i = i.strip()
+	main(i)
