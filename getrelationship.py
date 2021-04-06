74520bdd448eaa157dfcb81549a86b9f50a932ed
m4ll0k (mallok) <m4ll0k@protonmail.com>
2021-04-06 20:24:38 +0200

    Update getrelationship.py

--- getrelationship.py
+++ getrelationship.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import requests
 from lxml import html
 import os 
@@ -10,15 +8,17 @@ if not sys.version_info.major >= 3:
 
 def usage():
     print('Usage:\n')
-    print('\tpython3 {} domain1,domain2,domain3,..'.format(sys.argv[0]))
-    print('\tcat targets.txt | python3 {} '.format(sys.argv[0]))
-    print('\nby m4ll0k (@m4ll0k2)\n')
+    print('\tpython3 {} <domain1,domain2> <cookie>'.format(sys.argv[0]))
+    print('\tcat targets.txt | python3 {} <cookie>'.format(sys.argv[0]))
+    print('\nby m4ll0k (@m4ll0k)\n')
     sys.exit(0)
 
 
-def main(target: str) -> None:
+def main(target: str, cookie:str) -> None:
+    headers = {'authority': 'builtwith.com', 'cache-control': 'max-age=0', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7' }
+    headers['cookie'] = cookie
     try:
-        content = requests.get('https://builtwith.com/relationships/{0}'.format(target)).content
+        content = requests.get(f'https://builtwith.com/relationships/{target}',headers=headers).content
         html_ = html.fromstring(content)
         for target_ in html_.xpath('//td[@class="hbomb"]/a/text()'):
             print(target_)
@@ -31,11 +31,16 @@ def main(target: str) -> None:
 
 if len(sys.argv) > 1:
     for target in sys.argv[1].split(','):
-        main(target)
+        try:
+            main(target,sys.argv[2])
+        except Exception as err:
+            usage()
 else:
     for target in sys.stdin.readlines():
         t = target.strip()
         if target == '\n':
             usage()
-        main(t)
-
+        try:
+            main(t,sys.argv[1])
+        except Exception as err:
+            usage()
